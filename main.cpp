5e673633dafb26388b26751a478de53b84869857
Sofia Gaisina <129632311+ssupersonicc@users.noreply.github.com>
2023-09-22 21:36:39 +0300

    done lzw

--- main.cpp
+++ main.cpp
@@ -6,10 +6,38 @@
 #include <unordered_map>
 #include <vector>
 
-// может есть смысл сначала определять какой у нас алфавит?
-// нет сжатия размера
-
 typedef unsigned char uchar;
+const int MAX_SIZE = 16;
+
+void CreateDict(std::unordered_map<std::string, int>& dict) {
+  for (int i = 1; i <= (1 << 8); ++i) {
+    std::string symbol(1, static_cast<uchar>(i - 1));
+    dict[symbol] = i;
+  }
+  return;
+}
+
+void CreateDictReverse(std::unordered_map<int, std::string>& dict) {
+  for (int i = 1; i <= (1 << 8); ++i) {
+    std::string symbol(1, static_cast<uchar>(i - 1));
+    dict[i] = symbol;
+  }
+  return;
+}
+
+bool CheckDict(std::unordered_map<int, std::string>& dict, int& length) {
+  if (dict.size() == (1 << length) - 1) {
+    if (length == MAX_SIZE) {
+      dict.clear();
+      length = 9;
+      CreateDictReverse(dict);
+      return true;
+    } else {
+      ++length;
+    }
+  }
+  return false;
+}
 
 void GammaCode(int val, std::vector<bool>& vec) {
   std::vector<bool> temp;
@@ -21,6 +49,20 @@ void GammaCode(int val, std::vector<bool>& vec) {
     vec.push_back(0);
   }
   for (int i = temp.size() - 1; i >= 0; --i) {
+    vec.push_back(temp[i]);
+  }
+  return;
+}
+
+void CodeOutput(int val, int length, std::vector<bool>& vec) {
+  std::vector<bool> temp(length);
+  int pos = temp.size() - 1;
+  while (val != 0) {
+    temp[pos] = (val & 1);
+    val >>= 1;
+    --pos;
+  }
+  for (int i = 0; i < temp.size(); ++i) {
     vec.push_back(temp[i]);
   }
   return;
@@ -43,7 +85,6 @@ std::vector<bool> Encode_Unlimited(std::vector<uchar>& bytes) {
       prev = cur;
       cur += bytes[it];
     }
-    // std::cout << dict[prev] << '\n';
     GammaCode(dict[prev], bin);
     int value = dict.size() + 1;
     dict[cur] = value;
@@ -52,8 +93,6 @@ std::vector<bool> Encode_Unlimited(std::vector<uchar>& bytes) {
   }
   return bin;
 }
-
-void Decode_Limited() {}
 
 std::vector<uchar> Decode_Unlimited(std::vector<uchar>& bytes) {
   std::vector<uchar> output;
@@ -64,7 +103,6 @@ std::vector<uchar> Decode_Unlimited(std::vector<uchar>& bytes) {
   }
   std::vector<int> code;
   int it = 0;
-  std::vector<bool> value;
   std::vector<bool> bits;
   for (int i = 0; i < bytes.size(); ++i) {
     char byte = bytes[i];
@@ -72,7 +110,6 @@ std::vector<uchar> Decode_Unlimited(std::vector<uchar>& bytes) {
       bits.push_back(byte & (1 << i));
     }
   }
-  // ???
   while (it < bits.size()) {
     int zero = 0;
     int num = 0;
@@ -110,7 +147,88 @@ std::vector<uchar> Decode_Unlimited(std::vector<uchar>& bytes) {
 
 std::vector<bool> Encode_Limited(std::vector<uchar>& bytes) {
   std::vector<bool> bin;
+  int length = 9;
+
+  std::unordered_map<std::string, int> dict;
+  CreateDict(dict);
+  std::string cur = "";
+  std::string prev = "";
+  int it = 0;
+  while (it < bytes.size()) {
+    cur += bytes[it];
+    while ((dict.count(cur) != 0 || cur.length() == 0) && it < bytes.size()) {
+      ++it;
+      prev = cur;
+      cur += bytes[it];
+    }
+    CodeOutput(dict[prev], length, bin);
+    int value = dict.size() + 1;
+    dict[cur] = value;
+    if (dict.size() == (1 << length)) {
+      if (length != MAX_SIZE)
+        ++length;
+      else {
+        dict.clear();
+        length = 9;
+        CreateDict(dict);
+      }
+    }
+    cur.clear();
+    prev.clear();
+  }
   return bin;
+}
+
+std::vector<uchar> Decode_Limited(std::vector<uchar>& bytes) {
+  int length = 9;
+  std::vector<uchar> output;
+  std::unordered_map<int, std::string> dict;
+  CreateDictReverse(dict);
+
+  int it = 0;
+  std::vector<bool> bits;
+  for (int i = 0; i < bytes.size(); ++i) {
+    char byte = bytes[i];
+    for (int i = 7; i >= 0; --i) {
+      bits.push_back(byte & (1 << i));
+    }
+  }
+  std::string prev = "";
+  std::string cur = "";
+  while (it < bits.size()) {
+    int code = 0;
+    int pos = length - 1;
+    for (int i = 0; i < length && it < bits.size(); ++i) {
+      code |= bits[it] << pos;
+      --pos;
+      ++it;
+    }
+    if (pos == -1) {
+      bool f = false;
+      if (dict.count(code)) {
+        cur = dict[code];
+        int value = dict.size() + 1;
+        if (prev.length() != 0) {
+          dict[value] = prev + cur[0];
+          f = CheckDict(dict, length);
+        }
+      } else {
+        cur = prev + prev[0];
+        int value = dict.size() + 1;
+        dict[value] = cur;
+        f = CheckDict(dict, length);
+      }
+      if (f)
+        prev = "";
+      else
+        prev = cur;
+      for (int j = 0; j < cur.length(); ++j) {
+        output.push_back(static_cast<uchar>(cur[j]));
+        //std::cout << static_cast<uchar>(cur[j]) << '\n';
+      }
+    }
+  }
+  return output;
 }
 
 void PrintBin(std::string file_output, std::vector<bool>& bin) {
@@ -176,7 +294,7 @@ int main() {
     } else if (command == 2) {
       std::vector<uchar> output;
       if (type == 1) {
-        Decode_Limited();
+        output = Decode_Limited(bytes);
       } else if (type == 2) {
         output = Decode_Unlimited(bytes);
       }
